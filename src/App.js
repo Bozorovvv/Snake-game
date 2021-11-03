5388c17b17346c2c6229373fa06549ce6372ea00
Dyadya <farhod.jonikulovich@gmail.com>
2021-11-03 18:24:49 +0300

    must fix

--- src/App.js
+++ src/App.js
@@ -1,73 +1,74 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import Apple from "./components/Apple";
 import Snake from "./components/Snake";
 
-const getRandomCoordinates = () => {
+function getRandomCoordinates() {
   let min = 1;
   let max = 98;
   let x = Math.floor((Math.random() * (max - min + 1) * min) / 2) * 2;
   let y = Math.floor((Math.random() * (max - min + 1) * min) / 2) * 2;
   return [x, y];
-};
+}
 
-const initialState = {
-  score: 0,
-  highScore: localStorage.getItem("highestScore") || 0,
-  gameOver: false,
-  interval: undefined,
-  direction: "RIGHT",
-  speed: 200,
-  apple: getRandomCoordinates(),
-  snake: [
+export default function App() {
+  const [highScore, setHighScore] = useState(
+    localStorage.getItem("highestScore") || 0
+  );
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+  const [inte, setInte] = useState(undefined);
+  const [direction, setDirection] = useState("RIGHT");
+  const [speed, setSpeed] = useState(200);
+  const [apple, setApple] = useState(getRandomCoordinates());
+  const [snake, setSnake] = useState([
     [0, 0],
     [2, 0],
-  ],
-};
+  ]);
 
-class App extends Component {
-  state = initialState;
+  // useEffect(() => {
+  //   /// componentDidMount
+  //   document.onkeydown = onKeyDown;
+  //   setInterval(moveSnake, speed);
+  //   if (!gameOver) {
+  //     checkOutBorder();
+  //     checkCollapsed();
+  //     checkEat();
+  //   }
+  // }, []);
 
-  componentDidMount() {
-    const interval = setInterval(this.moveSnake, this.state.speed);
-    this.setState({ interval });
-    document.onkeydown = this.onKeyDown;
-  }
+  // // componentDidUpdate on snake change
+  // useEffect(() => {
+  //   // console.log(snake);
+  // }, [snake]);
 
-  componentDidUpdate() {
-    if (!this.state.gameOver) {
-      this.checkOutBorder();
-      this.checkCollapsed();
-      this.checkEat();
-    }
-  }
-
-  onKeyDown = (e) => {
+  function onKeyDown(e) {
     e = e || window.event;
     switch (e.keyCode) {
       case 38:
-        this.setState({ direction: "UP" });
+        setDirection("UP");
         break;
       case 40:
-        this.setState({ direction: "DOWN" });
+        setDirection("DOWN");
         break;
       case 37:
-        this.setState({ direction: "LEFT" });
+        setDirection("LEFT");
         break;
       case 39:
-        this.setState({ direction: "RIGHT" });
+        setDirection("RIGHT");
         break;
       case 32:
-        if (this.state.gameOver) {
-          this.startGame();
+        if (gameOver) {
+          startGame();
         }
         break;
     }
-  };
+  }
 
-  moveSnake = () => {
-    let dots = [...this.state.snake];
+  function moveSnake() {
+    console.log(snake);
+    let dots = [...snake];
     let head = dots[dots.length - 1];
-    switch (this.state.direction) {
+    switch (direction) {
       case "RIGHT":
         head = [head[0] + 2, head[1]];
         break;
@@ -82,118 +83,110 @@ class App extends Component {
         break;
     }
     dots.push(head);
+    console.log(dots);
     dots.shift();
-    this.setState({
-      snake: dots,
-    });
-  };
+    setSnake(dots);
+  }
 
-  checkCollapsed() {
-    let snake = [this.state.snake];
+  function checkCollapsed() {
     let head = snake[snake.length - 1];
     snake.pop();
     snake.forEach((dot) => {
       if (head[0] == dot[0] && head[1] == dot[1]) {
-        this.gameOver();
+        onGameOver();
       }
     });
   }
 
-  checkOutBorder() {
-    let head = this.state.snake[this.state.snake.length - 1];
+  function checkOutBorder() {
+    let head = snake[snake.length - 1];
     if (head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0) {
-      this.gameOver();
+      onGameOver();
     }
   }
 
-  checkEat() {
-    let head = this.state.snake[this.state.snake.length - 1];
-    let apple = this.state.apple;
+  function checkEat() {
+    let head = snake[snake.length - 1];
     if (head[0] == apple[0] && head[1] == apple[1]) {
-      this.setState({
-        apple: getRandomCoordinates(),
-      });
-      this.enlargeSnake();
-      this.increaseSpeed();
-      this.changeScores();
+      setApple(getRandomCoordinates());
+      enlargeSnake();
+      increaseSpeed();
+      changeScores();
     }
   }
 
-  enlargeSnake() {
-    let newSnake = [...this.state.snake];
+  function enlargeSnake() {
+    let newSnake = [...snake];
     newSnake.unshift([]);
-    this.setState({
-      snake: newSnake,
-    });
+    setSnake(newSnake);
   }
 
-  increaseSpeed() {
-    if (this.state.speed > 10) {
-      this.setState({
-        speed: this.state.speed - 10,
-      });
+  function increaseSpeed() {
+    if (speed > 10) {
+      setSpeed((speed -= 10));
     }
   }
 
-  startGame() {
-    const { highScore } = this.state;
-    this.setState(initialState);
-    this.setState({ highScore });
-    const interval = setInterval(this.moveSnake, this.state.speed);
-    this.setState({ interval });
+  function startGame() {
+    setInte(undefined);
+    setDirection("RIGHT");
+    setSpeed(200);
+    setApple(getRandomCoordinates());
+    setSnake([
+      [0, 0],
+      [2, 0],
+    ]);
+    setScore(0);
+    setGameOver(false);
+    setHighScore(highScore);
+    setInterval(moveSnake, speed);
   }
 
-  gameOver() {
-    clearInterval(this.state.interval);
-    this.setState({
-      gameOver: true,
-      interval: undefined,
-      direction: "RIGHT",
-      speed: 200,
-      apple: getRandomCoordinates(),
-      snake: [
-        [0, 0],
-        [2, 0],
-      ],
-    });
+  function onGameOver() {
+    clearInterval(inte);
+    setGameOver(true);
+    setInte(undefined);
+    setDirection("RIGHT");
+    setSpeed(200);
+    setApple(getRandomCoordinates());
+    setSnake([
+      [0, 0],
+      [2, 0],
+    ]);
   }
 
-  changeScores() {
-    let { score, highScore } = this.state;
+  function changeScores() {
     score += 1;
     if (score > highScore) {
       highScore = score;
     }
-    this.setState({ score, highScore });
+    setHighScore(highScore);
+    setScore(score);
   }
 
-  render() {
-    return (
-      <div className="app-area">
-        <div className="app">
-          <div>
-            <Snake snakeLocation={this.state.snake} />
-            {this.state.gameOver ? (
-              <>
-                <div className="game-over">GAME OVER</div>
-                <div className="space">press SPACE to start</div>
-              </>
-            ) : null}
-            <Apple appleLocation={this.state.apple} />
-          </div>
-        </div>
-        <div className="score">
-          <h1>Snake game</h1>
-          <h2>
-            High score: <p>{this.state.highScore}</p>
-          </h2>
-          <h2>
-            Your score: <p>{this.state.score}</p>
-          </h2>
+  return (
+    <div className="app-area">
+      <div className="app">
+        <div>
+          <Snake snakeLocation={snake} />
+          {gameOver ? (
+            <>
+              <div className="game-over">GAME OVER</div>
+              <div className="space">press SPACE to start</div>
+            </>
+          ) : null}
+          <Apple appleLocation={apple} />
         </div>
       </div>
-    );
-  }
+      <div className="score">
+        <h1>Snake game</h1>
+        <h2>
+          High score: <p>{highScore}</p>
+        </h2>
+        <h2>
+          Your score: <p>{score}</p>
+        </h2>
+      </div>
+    </div>
+  );
 }
-
-export default App;
