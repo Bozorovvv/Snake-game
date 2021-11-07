2b74a45e7ec00b91fe0f5c98fa157c5c69d69ced
farhod <farhod.gmail.com>
2021-11-07 22:30:38 +0500

    tugadi

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,73 +1,78 @@
-import React, { useState, useEffect } from "react";
-import GameArea from "./GameArea";
-import Settings from "./Settings";
-import Statistics from "./Statistics";
-import WinnerSound from "../sounds/win.wav";
-import BackgroundSound from "../sounds/chael.mp3";
-import GameOverSound from "../sounds/gameover.wav";
+import React, { useState, useEffect } from 'react'
+import GameArea from './GameArea'
+import Settings from './Settings'
+import Statistics from './Statistics'
+import WinnerSound from '../sounds/win.wav'
+import BackgroundSound from '../sounds/chael.mp3'
+import GameOverSound from '../sounds/gameover.wav'
 
 function Game() {
-  const [snakeColor, setSnakeColor] = useState("#309bff");
-  const [appleColor, setAppleColor] = useState("#ff4830");
-  const [areaColor, setAreaColor] = useState("#fff");
-  const [musicIs, setMusicIs] = useState(false);
-  const [soundEffects, setSoundEffects] = useState(false);
-  const [gameSpeed, setGameSpeed] = useState(200);
-  const gameMusic = new Audio(BackgroundSound);
-  const winnerSound = new Audio(WinnerSound);
-  const gameOverSound = new Audio(GameOverSound);
+  const [snakeColor, setSnakeColor] = useState('#309bff')
+  const [appleColor, setAppleColor] = useState('#ff4830')
+  const [areaColor, setAreaColor] = useState('#fff')
+  const [musicIs, setMusicIs] = useState(false)
+  const [soundEffects, setSoundEffects] = useState(false)
+  const [gameSpeed, setGameSpeed] = useState(200)
+  const [gameScore, setGameScore] = useState(false)
+  const gameMusic = new Audio(BackgroundSound)
+  const winnerSound = new Audio(WinnerSound)
+  const gameOverSound = new Audio(GameOverSound)
 
   useEffect(() => {
-    handlePlayMusic();
-  }, [musicIs]);
+    handlePlayMusic()
+  }, [musicIs])
 
   function changeSnakeColor(e) {
-    setSnakeColor(e.target.value);
+    setSnakeColor(e.target.value)
   }
   function changeAppleColor(e) {
-    setAppleColor(e.target.value);
+    setAppleColor(e.target.value)
   }
   function changeAreaColor(e) {
-    setAreaColor(e.target.value);
+    setAreaColor(e.target.value)
   }
 
   function handleMusic() {
-    setMusicIs(!musicIs);
-    gameMusic.pause();
+    setMusicIs(!musicIs)
+    gameMusic.pause()
   }
 
   function handleEffect() {
-    setSoundEffects(!soundEffects);
+    setSoundEffects(!soundEffects)
   }
 
   function handlePlayMusic() {
     if (musicIs) {
-      gameMusic.play();
+      gameMusic.play()
     }
   }
 
   function foodSound() {
     if (soundEffects) {
-      winnerSound.play();
+      winnerSound.play()
     }
   }
 
   function endGameSound() {
     if (soundEffects) {
-      gameOverSound.play();
+      gameOverSound.play()
     }
   }
 
+  function handleGameScore() {
+    setGameScore(!gameScore)
+  }
+
   function changeGameSpeed(e) {
-    let speed = e.target.value;
+    let speed = e.target.value
     if (speed == 0) {
-      setGameSpeed(200);
+      setGameSpeed(200)
     } else if (speed == 1) {
-      setGameSpeed(150);
+      setGameSpeed(150)
     } else if (speed == 2) {
-      setGameSpeed(100);
-    } else if (speed == 3) {
-      setGameSpeed(50);
+      setGameSpeed(100)
+    } else {
+      setGameSpeed(50)
     }
   }
 
@@ -92,6 +97,7 @@ function Game() {
         <div className="col">
           <GameArea
             foodSound={foodSound}
+            handleGameScore={handleGameScore}
             endGameSound={endGameSound}
             snakeColor={snakeColor}
             appleColor={appleColor}
@@ -100,11 +106,11 @@ function Game() {
           />
         </div>
         <div className="col">
-          <Statistics />
+          <Statistics gameScore={gameScore} />
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Game;
+export default Game
--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,17 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
 
-function Statistics({highScore, score}) {
+function Statistics({ gameScore }) {
+  const [data, setData] = useState([])
+
+  function handleResetStats() {
+    localStorage.removeItem('Scores')
+    setData([])
+  }
+
+  useEffect(() => {
+    let scores = localStorage.getItem('Scores')
+    if (scores !== null) {
+      scores = scores.split(',')
+      if (scores.length >= 10) {
+        while (scores.length !== 10) {
+          scores.shift()
+        }
+      }
+      setData(scores)
+      localStorage.setItem('Scores', scores.toString())
+    }
+  }, [gameScore])
+
   return (
-    <div className="score">
-      <h1>Snake game</h1>
-      <h2>
-        High score: <p>{highScore}</p>
-      </h2>
-      <h2>
-        Your score: <p>{score}</p>
-      </h2>
-    </div>
-  );
+    <>
+      <button
+        style={{ width: '100%' }}
+        className={'btn btn-danger'}
+        onClick={() => handleResetStats()}
+      >
+        Clear Stats
+      </button>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((el, index) => {
+            return el !== 'null' ? (
+              <tr key={index}>
+                <td>{index}</td>
+                <td>{el}</td>
+              </tr>
+            ) : null
+          })}
+        </tbody>
+      </table>
+    </>
+  )
 }
 
-export default Statistics;
+export default Statistics
